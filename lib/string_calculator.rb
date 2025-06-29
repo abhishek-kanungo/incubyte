cbb759e649b17e02e1d02c6c490c845f254d0023
Abhishek Kanungo <17295631+abhishek-kanungo@users.noreply.github.com>
2025-06-29 23:28:14 +0530

    test: added support for multiple numbers and newline as delimeter

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,6 +1,15 @@
 class StringCalculator
     
     def add input
-        0
+       parse_string_numbers(input).sum
+    end
+    
+    
+    private
+    
+    def parse_string_numbers(string)
+        return [] if string.empty?
+        
+        string.split(/,|\n/).map(&:to_i)
     end
 end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -3,11 +3,43 @@ require_relative "../lib/string_calculator"
 RSpec.describe StringCalculator do
     
     subject(:calculator) { StringCalculator.new }
-
     
     describe "add" do
-        it "returns 0 when input is an empty string" do
-            expect(calculator.add("")).to eq(0)
+        context "when given an empty string" do
+            it "returns 0 when input is an empty string" do
+                expect(calculator.add("")).to eq(0)
+            end
         end
+        
+        context "when given a single number as a string" do
+            it "returns the same number as integer" do
+                expect(calculator.add('5')).to eq(5)
+            end
+        end
+        
+        context "when given two comma separated numbers as string" do
+            it "returns their sum as integer" do
+                expect(calculator.add('5,10')).to eq(15)
+            end
+        end
+        
+        context "when givem muptiple comma separated numbers as string" do
+            it "returns their sum as integer" do
+                expect(calculator.add('5,10,20,100')).to eq(135)
+            end
+        end
+        
+        context "when given two newline separated numbers as string" do
+            it "returns their sum as integer" do
+                expect(calculator.add("5\n10")).to eq(15)
+            end
+        end
+        
+        context "when given multiple numbers separated by combination of newline and comma as string" do
+            it "returns their sum as integer" do
+                expect(calculator.add("5\n10,50")).to eq(65)
+            end
+        end
+        
     end
 end
